aea29ff2f7c143b60b18576989abbf890e31a8c0
Vanessa <vanessambeattie@gmail.com>
2023-07-27 19:05:38 -0500

    HM Complete

--- src/pages/EmployeePage.js
+++ src/pages/EmployeePage.js
@@ -1,9 +1,17 @@
 import EPHeader from "../components/EPHeader";
+import styles from '../components/Styles.module.css'
 
 function EmployeePage() {
     return (
-        <div className="EmployeePage">
+        <div className={styles.EmployeePage}>
             <EPHeader />
+            <ul>
+                <img src="https://img.freepik.com/premium-vector/man-profile-cartoon_18591-58482.jpg?w=1060" alt="React Image" />
+                <div className='Images'> 
+                    <h3>James King</h3>
+                    <h4>President</h4>
+                </div>
+            </ul>
             
         </div>
     )
